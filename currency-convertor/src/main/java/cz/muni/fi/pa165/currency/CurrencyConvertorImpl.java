2d05cfbca760436322d71b73796a88f3afe330be
Martin Geletka <xgeletka@fi.muni.cz>
2018-10-01 17:55:51 +0200

    seminar02 implemented

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.currency;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 
@@ -18,9 +19,30 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
         this.exchangeRateTable = exchangeRateTable;
     }
 
+    private void validateArgumentsForConvert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) throws IllegalArgumentException{
+        if(sourceCurrency == null){
+            throw new IllegalArgumentException("Source currency cannot be null");
+        }
+
+        if(targetCurrency == null){
+            throw new IllegalArgumentException("Target currency cannot be null");
+        }
+
+        if(sourceAmount == null){
+            throw new IllegalArgumentException("Source amount cannot be null");
+        }
+    }
+
     @Override
-    public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) throws  IllegalArgumentException{
+        BigDecimal result;
+        try {
+            validateArgumentsForConvert(sourceCurrency, targetCurrency, sourceAmount);
+            result = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency).multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
+        } catch (ExternalServiceFailureException ex){
+            throw new IllegalArgumentException("External error occuried");
+        }
+        return result;
     }
 
 }
--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,39 +1,84 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+
 import static org.junit.Assert.*;
 
 public class CurrencyConvertorImplTest {
 
-    @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+    private static BigDecimal EXCHANGE_RATE_STANDARD = new BigDecimal("25.5");
+    private static BigDecimal AMOUNT_OF_MONEY_STANDARD = new BigDecimal("300");
+    private static BigDecimal AMOUNT_OF_MONEY_NEGATIVE = new BigDecimal("300");
+
+    private static Currency CURRENCY_US = Currency.getInstance(Locale.US);
+    private static Currency CURRENCY_UK = Currency.getInstance(Locale.UK);
+    private static Currency CURRENCY_CANADA = Currency.getInstance(Locale.CANADA);
+
+    private ExchangeRateTable exchangeRateTable = Mockito.mock(ExchangeRateTable.class);
+    private CurrencyConvertor currencyConvertor;
+
+    @Before
+    public void setUp() throws ExternalServiceFailureException {
+        Mockito.when(exchangeRateTable.getExchangeRate(CURRENCY_US, CURRENCY_UK)).thenReturn(EXCHANGE_RATE_STANDARD);
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
     }
 
     @Test
-    public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+    public void testConvert() throws ExternalServiceFailureException {
+        BigDecimal expectedResult = EXCHANGE_RATE_STANDARD.multiply(AMOUNT_OF_MONEY_STANDARD);
+        BigDecimal actualResult = currencyConvertor.convert(CURRENCY_US, CURRENCY_UK, AMOUNT_OF_MONEY_STANDARD);
+        Assert.assertEquals(actualResult, expectedResult);
+
+        actualResult = currencyConvertor.convert(CURRENCY_US, CURRENCY_UK, BigDecimal.ZERO);
+        Assert.assertEquals(actualResult, BigDecimal.ZERO);
+
+        expectedResult = EXCHANGE_RATE_STANDARD.multiply(AMOUNT_OF_MONEY_NEGATIVE);
+        actualResult = currencyConvertor.convert(CURRENCY_US, CURRENCY_UK, AMOUNT_OF_MONEY_NEGATIVE);
+        Assert.assertEquals(actualResult, expectedResult);
+
+        BigDecimal longExchangeRate = new BigDecimal("3.14159");
+        Mockito.when(exchangeRateTable.getExchangeRate(CURRENCY_US, CURRENCY_CANADA)).thenReturn(longExchangeRate);
+        actualResult = currencyConvertor.convert(CURRENCY_US, CURRENCY_CANADA, AMOUNT_OF_MONEY_STANDARD);
+        expectedResult = longExchangeRate.multiply(AMOUNT_OF_MONEY_STANDARD);
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
-    @Test
+    @Test(expected = IllegalAccessException.class)
+    public void testConvertWithNullSourceCurrency()
+    {
+       currencyConvertor.convert(null, CURRENCY_UK, AMOUNT_OF_MONEY_STANDARD);
+    }
+
+    @Test(expected = IllegalAccessException.class)
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(CURRENCY_US, null, AMOUNT_OF_MONEY_STANDARD);
     }
 
-    @Test
+    @Test(expected = IllegalAccessException.class)
     public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(CURRENCY_US, CURRENCY_UK, null);
     }
 
-    @Test
+    @Test(expected = UnknownExchangeRateException.class)
     public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
+        Currency unknownCurrency = Currency.getInstance(Locale.CHINA);
+        currencyConvertor.convert(CURRENCY_US, unknownCurrency, AMOUNT_OF_MONEY_STANDARD);
     }
 
     @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException{
+        ExchangeRateTable badExchangeRateTable = Mockito.mock(ExchangeRateTable.class);
+        Mockito.when(badExchangeRateTable.getExchangeRate(CURRENCY_CANADA, CURRENCY_UK)).thenThrow(ExternalServiceFailureException.class);
+        currencyConvertor = new CurrencyConvertorImpl(badExchangeRateTable);
+        currencyConvertor.convert(CURRENCY_US, CURRENCY_UK, AMOUNT_OF_MONEY_STANDARD);
     }
 
 }
